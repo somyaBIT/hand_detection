4f1dcc141b2d7d4eec59cc835dd58cc31b884ee5
somyaBit <somya4287@gmail.com>
2021-03-18 13:40:58 +0530

    working

---
+++ app.js
@@ -0,0 +1,50 @@
+// Loading the model: handTrack.load()
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+  }
+
+
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia ||navigator.msGetUserMedia;
+
+// select everything in my html
+
+const video = document.querySelector('video');
+const audio = document.querySelector('audio');
+const canvas = document.querySelector('canvas');
+const context = canvas.getContext('2d');
+let model;
+
+handTrack.startVideo(video).then( status => {
+     if(status){
+         navigator.getUserMedia({video:{}}, stream => {
+             video.srcObject = stream;
+             setInterval(runDetectaion,1000); },
+         err => console.log(err)
+         );
+     }
+});
+
+function runDetectaion(){
+
+    model.detect(video)
+    .then(predictions => {
+
+        console.log(predictions);
+        // to draw bounding boxes on a canvas element and customizable model parameters.
+        model.renderPredictions(predictions,canvas,context,video);  
+        if(predictions.length>0)
+        {
+            audio.play()
+        }    
+    });
+}
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+    model = lmodel;
+  });
+
